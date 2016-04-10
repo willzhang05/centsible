a8a958cf4e8935eb9bf05b526987e552d7adeb7b
William Zhang <willzhang05@gmail.com>
2016-04-10 10:04:45 -0400

    Settings update

---
+++ settings.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+    var amount = document.getElementById("money");
+    amount.style.fontSize = "16px";
+    chrome.storage.sync.get("balance", function (balance) {
+        if(balance.balance < 0) {
+            amount.style.color = "red";
+        } else {
+            amount.innerHTML = balance.balance;
+            if(amount.innerHTML.length == 1) {
+                amount.innerHTML = balance.balance + "00";
+            }
+            amount.innerHTML = "Current Balance in Account: $" + (amount.innerHTML).substring(0, (amount.innerHTML).length - 2) + "." + (amount.innerHTML).substring((amount.innerHTML).length - 2)
+            amount.style.color = "green";
+        }
+    });
+    var toggle = document.getElementById("toggle-box");
+    chrome.storage.sync.get("disabled", function (bool) {
+        if(chrome.runtime.lastError) {
+            toggle.checked = true;
+            return;
+        }
+        console.log(bool.disabled);
+        toggle.checked = bool.disabled;
+        setToggleValue(toggle.checked);
+    });
+    toggle.addEventListener("click", function() {
+        setToggleValue(toggle.checked);
+    });
+    var idBox = document.getElementById("capitalone-id");
+    document.getElementById("submit").addEventListener("click", function() {
+        chrome.storage.sync.set({"id" : idBox.value}, function() {
+            console.log("Saved value " + idBox.value + " to Chrome Storage");
+        });
+    });
+}
+
+function setToggleValue(bool) {
+    chrome.storage.sync.set({"disabled" : bool}, function() {
+        console.log("Saved toggle state " + bool + " to Chrome Storage");
+     });
+}
