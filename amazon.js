1cf5015e5806471bd07bdded70a555afdc40c3ae
William Zhang <willzhang05@gmail.com>
2016-04-10 01:48:41 -0400

    Many updates to stuff; amazon disabling

--- amazon.js
+++ amazon.js
@@ -1,7 +1,31 @@
 var balance;
 var id = "57092e1b319313dd1b4310e8";
-$.get("http://api.reimaginebanking.com/customers/" + id + "/accounts?key=7183aadcbc7ff9d602fb63db7fe93f2b", function(data) {
-    var json = JSON.parse(data);
+$.get("https://sysadmin.wzhang.me/" + id + "/accounts?key=7183aadcbc7ff9d602fb63db7fe93f2b", function(data) {
+    var json = data[0]; //.substring(1, data.length - 1));
+    console.log(json);
     var balance = parseInt(json["balance"]);
     console.log(balance);
+    
+    var price = document.getElementsByClassName("a-size-medium a-color-price sc-price sc-white-space-nowrap  sc-price-sign")[0].innerHTML;
+    price = parseInt(price.substring(1, price.length).replace(".", ""));
+    console.log(balance - price);
+    if(balance - price < 0) {
+        var isDisabled;
+        chrome.storage.sync.get("disabled", function (bool) {
+            console.log(bool);
+            isDisabled = bool;
+        });
+        if(!isDisabled) {
+            var checkoutButtons = document.getElementsByName("proceedToCheckout");
+            console.log(checkoutButtons[0]);
+            for(var i = 0; i < checkoutButtons.length; i++) {
+                console.log(checkoutButtons[i]);
+                checkoutButtons[i].disabled = true;
+                checkoutButtons[i].style.opacity = "0.2";
+            }
+        } else {
+            checkoutButtons[i].disabled = false;
+            checkoutButtons[i].style.opacity = "1.0";
+        }
+    }
 });
--- popup.js
+++ popup.js
@@ -2,23 +2,40 @@ var disabled = false;
 window.onload = function() {
     var settings = document.getElementById("settings");
     console.log(settings);
-    settings.addEventListener('click', function() {
+    settings.addEventListener("click", function() {
         var w = window.open("settings.html", "_blank");
         w.focus();
     });
     var toggle = document.getElementById("toggle-box");
-    toggle.addEventListener('click', function() {
-        disabled = toggle.checked;
-        chrome.storage.sync.set({'disabled' : disabled}, function() {
-            console.log(disabled);
-        });
+    chrome.storage.sync.get("disabled", function (bool) {
+        if(chrome.runtime.lastError) {
+            toggle.checked = true;
+            return;
+        }
+        console.log(bool.disabled);
+        toggle.checked = bool.disabled;
+        setToggleValue(toggle.checked);
+    });
+    toggle.addEventListener("click", function() {
+       //*if(isToggled)V {
+         /*   toggle.checked = "checked";
+        }
+`       
+    disabled = toggle.checked;*/
+        //chrome.storage.sync.set({"disabled" : toggle.checked}, function() {
+            /*chrome.storage.sync.get("disabled", function (bool) {
+                console.log(bool);
+            });*/
+            setToggleValue(toggle.checked);
+        //});
         //document.getElementById("toggle-form").submit();
     });
 }
 
-
-function getAccountInfoFromId(id) {
-    
+function setToggleValue(bool) {
+        chrome.storage.sync.set({"disabled" : bool}, function() {
+            console.log("Saved toggle state " + bool + " to Chrome Storage");
+        });
 }
 
 /*(function() {
